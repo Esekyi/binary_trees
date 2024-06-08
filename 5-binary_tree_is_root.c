895a16b45d98df4c87bbc22dd56b79b4bd52e58f
[esekyi] <[sskert10@gmail.com]>
2024-06-08 04:20:01 +0000

    6. Pre-order traversal

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 
 /**
- * binary_tree_is_root - checks if a given node is the root of a binary tree
- * @node: pointer to the node to check
- *
- * Return: 1 if node is the root, otherwise 0
+ * binary_tree_is_root - check the code
+ * @node: pointer to the node
+ * Description: checks if a node is the root of a binary tree
+ * Return: 1 if node is the root and 0 otherwise
  */
+
 int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL || node->parent != NULL)
